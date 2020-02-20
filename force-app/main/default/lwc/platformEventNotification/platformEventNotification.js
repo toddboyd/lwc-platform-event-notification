04af12b324f8072abd780e957a628c7d0bfc8353
Todd Boyd <todd.boyd@gmail.com>
2020-02-19 17:25:24 -0700

    Add user and application filtering

--- force-app/main/default/lwc/platformEventNotification/platformEventNotification.js
+++ force-app/main/default/lwc/platformEventNotification/platformEventNotification.js
@@ -1,19 +1,27 @@
 import { LightningElement, track, api } from "lwc";
 import { ShowToastEvent } from "lightning/platformShowToastEvent";
 import { subscribe, unsubscribe } from "lightning/empApi";
+import Id from "@salesforce/user/Id";
 
 export default class PlatformEventNotification extends LightningElement {
   @track channelName = "/event/ClientNotification__e";
   @api applicationName;
+  @api isUserSpecific;
+  userId = Id;
   subscription = {};
 
-  showToast(title, message, variant) {
-    const evt = new ShowToastEvent({
-      title: title,
-      message: message,
-      variant: variant
-    });
-    this.dispatchEvent(evt);
+  showToast(application, title, message, variant, user) {
+    if (
+      (user == null || user === this.userId) &&
+      (application == null || application === this.applicationName)
+    ) {
+      const evt = new ShowToastEvent({
+        title: title,
+        message: message,
+        variant: variant
+      });
+      this.dispatchEvent(evt);
+    }
   }
 
   connectedCallback() {
@@ -21,8 +29,10 @@ export default class PlatformEventNotification extends LightningElement {
       const message = response.data.payload;
       this.showToast(
         message.Application__c,
+        message.Title__c,
         message.Message__c,
-        message.Type__c
+        message.Type__c,
+        message.User__c
       );
     }.bind(this);
     subscribe(this.channelName, -1, messageCallback).then(response => {
